d8d37cdb5597e4ae3b5a87bde93936b09a513f47
Mike Taylor <mike@indexdata.com>
2017-07-03 15:54:24 +0100

    Get rid of obsolete eslint-disable-next-line import/no-unresolved comments. Part of STRIPES-429

--- plugin-markdown-better/index.js
+++ plugin-markdown-better/index.js
@@ -1,5 +1,3 @@
-// We have to remove node_modules/react to avoid having multiple copies loaded.
-// eslint-disable-next-line import/no-unresolved
 import React from 'react';
 
 export default () => <div>Better Markdown editor goes here</div>;
